f3c3d184eda8e233055daaa3ae1db0200f8d9c16
Sheba1990 <57401985+Sheba1990@users.noreply.github.com>
2025-04-27 11:24:37 +0300

    f

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import sys
 import random
 from math import *
 
-from pygame.camera import Camera
+from Tools.demo.sortvisu import steps
 
 pygame.init()
 current_path = os.path.dirname(__file__)
@@ -13,36 +13,50 @@ WIDTH = 1200
 HEIGHT = 800
 FPS = 60
 
+
+
 sc = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
+from pygame.camera import Camera
+
+from kartinki import *
 
 
 def restart():
-    global player_group, road_group, grass_group, asphalt_group, camera_group
+    global player_group, road_group, grass_group, asphalt_group, camera_group,player,start_group
     player_group = pygame.sprite.Group()
     road_group = pygame.sprite.Group()
     grass_group = pygame.sprite.Group()
-    camera_group = SuperGroup()
+    camera_group = pygame.sprite.Group()
     asphalt_group = pygame.sprite.Group()
-
-
-
+    start_group = pygame.sprite.Group()
+    player = Player(player_image, (50,50))
+    player_group.add(player)
 
 def gamelvl():
-    sc.fill('dimgray')
+    sc.blit(fon_image,(0,0))
+    sc.blit(taxmetr_image, (300,600))
+    sc.blit(speedmetr_image, (600, 600))
+    road_group.draw(sc)
+    road_group.update(0)
+    asphalt_group.draw(sc)
+    asphalt_group.update(0)
+    grass_group.draw(sc)
+    grass_group.update(0)
+    start_group.draw(sc)
+    start_group.update(0)
+
     player_group.draw(sc)
     player_group.update()
-    road_group.draw(sc)
-    road_group.update()
-    asphalt_group.draw(sc)
-    asphalt_group.update()
 
+
+    pygame.display.update()
 
 
 def drawmaps(nameFile):
     maps = []
-    source = "lvl/" + str(nameFile)
+    source = "karta/" + str(nameFile)
     with open(source, "r") as file:
         for i in range(0, 5):
             maps.append(file.readline().replace("\n", "").split(",")[0:-1])
@@ -64,37 +78,101 @@ def drawmaps(nameFile):
             elif maps[i][j] == '3':
                 asphalt = Asphalt(asphalt_image, pos)
                 asphalt_group.add(asphalt)
+                camera_group.add(asphalt)
+            elif maps[i][j] == '4':
+                start = Start(start_image, pos)
+                start_group.add(start)
+                camera_group.add(start)
+
+
+class Road(pygame.sprite.Sprite):
+    def __init__(self,image,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self, step):
+        self.rect.x += step
 
 
 
 
+class Grass(pygame.sprite.Sprite):
+    def __init__(self,image,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self, step):
+        self.rect.x += step
+
+class Start(pygame.sprite.Sprite):
+    def __init__(self,image,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self, step):
+        self.rect.x += step
+
+class Asphalt(pygame.sprite.Sprite):
+    def __init__(self,image,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self, step):
+        self.rect.x += step
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,image,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.speed = 1
 
 
 
+    def update(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_d]:
+            self.rect.x += self.speed
+            if self.rect.right > WIDTH/2 + 500:
+                self.rect.right = WIDTH/2 + 500
+                camera_group.update(-self.speed)
 
+        elif key[pygame.K_a]:
+            self.rect.x -= self.speed
+            if self.rect.right < WIDTH/2 - 500:
+                self.rect.right = WIDTH/2 - 500
+                camera_group.update(self.speed)
 
+        if key[pygame.K_1]:
+            self.speed = 3
+        elif key[pygame.K_2]:
+            self.speed = 5
+        elif key[pygame.K_3]:
+            self.speed = 7
+        elif key[pygame.K_4]:
+            self.speed = 12
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+restart()
+drawmaps('Drag.txt')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    'gamelvl()'
+    gamelvl()
     clock.tick(FPS)
---
+++ kartinki.py
@@ -0,0 +1,14 @@
+import pygame.image
+
+road_image = pygame.image.load('image/road.png').convert_alpha()
+asphalt_image = pygame.image.load('image/asphalt.png').convert_alpha()
+grass_image = pygame.image.load('image/grass.png').convert_alpha()
+player_image = pygame.image.load('image/player.png').convert_alpha()
+fon_image = pygame.image.load('image/fon.jpg').convert_alpha()
+start_image = pygame.image.load('image/start-finish.png').convert_alpha()
+speedmetr_image = pygame.image.load('image/speedometr.png').convert_alpha()
+taxmetr_image = pygame.image.load('image/taxometr.png').convert_alpha()
+
+
+
+
